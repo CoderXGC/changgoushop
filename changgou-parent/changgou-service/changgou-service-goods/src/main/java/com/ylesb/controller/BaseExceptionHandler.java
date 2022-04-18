1306daa93a191c82546fe37ce701c2f30a99210c
CoderXGC <xgc@tom.com>
2022-04-18 08:16:53 +0800

    init

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/ylesb/controller/BaseExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.ylesb.controller;
+/**
+ * @title: BaseExceptionHandler
+ * @projectName changgou
+ * @description: TODO
+ * @author White
+ * @site : [www.ylesb.com]
+ * @date 2022/4/1720:29
+ */
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @className    : BaseExceptionHandler
+ * @description  : [描述说明该类的功能]  
+ * @author       : [XuGuangchao]
+ * @site         : [www.ylesb.com]
+ * @version      : [v1.0]
+ * @createTime   : [2022/4/17 20:29]
+ * @updateUser   : [XuGuangchao]
+ * @updateTime   : [2022/4/17 20:29]
+ * @updateRemark : [描述说明本次修改内容] 
+ */
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    /***
+     * 异常处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+}
